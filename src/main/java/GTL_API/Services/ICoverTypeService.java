3b242ef33e5b0b46ac153a9ba4c9e199db6c93b1
Jakub Kida <1067677@ucn.dk>
2020-04-24 18:41:26 +0200

    Update travis file

--- src/main/java/GTL_API/Services/ICoverTypeService.java
+++ src/main/java/GTL_API/Services/ICoverTypeService.java
@@ -7,5 +7,6 @@ import GTL_API.Models.UpdateModels.CoverTypeUpdate;
 public interface ICoverTypeService {
     CoverTypeReturn findCoverTypeByName(String coverType);
 
+
     CoverTypeReturn updateCoverType(CoverTypeUpdate coverType);
 }
