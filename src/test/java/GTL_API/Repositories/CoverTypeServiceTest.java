aaad337adc2e87ea03c00de97df45b86ecf1701e
Jakub Kida <1067677@ucn.dk>
2020-04-24 18:28:15 +0200

    Adding travis CI config file and CoverTypeServiceTest.java

---
+++ src/test/java/GTL_API/Repositories/CoverTypeServiceTest.java
@@ -0,0 +1,42 @@
+package GTL_API.Repositories;
+
+import GTL_API.Models.ReturnModels.CoverTypeReturn;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+public class CoverTypeServiceTest {
+
+    private CoverTypeReturn coverTypeReturn;
+
+    @Before
+    public void setUp() {
+        coverTypeReturn = new CoverTypeReturn();
+        coverTypeReturn.setCoverType("TestCoverTypeName");
+
+
+    }
+
+    @MockBean
+    private CoverType coverTypeRepository;
+
+
+    @Test
+    public void whenFindByCoverTypeName_thenReturnCoverTypeReturn() {
+        Mockito.when(coverTypeRepository.findCoverTypeByName(Mockito.anyString()))
+                .thenReturn(this.coverTypeReturn);
+
+        CoverTypeReturn found = coverTypeRepository.findCoverTypeByName("ASD");
+
+
+        Assert.assertEquals("TestCoverTypeName", found.getCoverType());
+    }
+
+}
