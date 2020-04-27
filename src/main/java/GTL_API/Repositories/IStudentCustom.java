3fa600dfc538d117b55f958b3f1d42f66d448109
eviluser <jendrekpb@gmail.com>
2020-04-27 15:38:24 +0200

    changed return type in IStudentCustom

--- src/main/java/GTL_API/Repositories/IStudentCustom.java
+++ src/main/java/GTL_API/Repositories/IStudentCustom.java
@@ -6,6 +6,6 @@ import GTL_API.Models.ReturnModels.StudentReturn;
 import java.util.Optional;
 
 public interface IStudentCustom {
-    Optional<StudentEntity> findByStudentId(int studentId);
-    Optional<StudentEntity> findByGpaBetween(int bottomRange, int upperRange);
+    StudentReturn findByStudentId(int studentId);
+    StudentReturn findByGpaBetween(int bottomRange, int upperRange);
 }
