de625c3e011cea15c4e40b042394874d736fbe65
Czubbi <piotr.gzubicki97@gmail.com>
2020-04-30 15:57:09 +0200

    Data source config

--- src/main/java/GTL_API/Handlers/DataSourceConfig.java
+++ src/main/java/GTL_API/Handlers/DataSourceConfig.java
@@ -16,9 +16,9 @@ public class DataSourceConfig {
     public DataSource dataSourceTest() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSource.setUrl("jdbc:sqlserver://localhost;database=Giorgia_Tech_Library");
+        dataSource.setUrl("jdbc:sqlserver://64.225.69.54;database=Giorgia_Tech_Library");
         dataSource.setUsername("sa");
-        dataSource.setPassword("123");
+        dataSource.setPassword("Password123");
         return dataSource;
     }
 
@@ -26,7 +26,7 @@ public class DataSourceConfig {
     public DataSource dataSourceLibrarian(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSource.setUrl("jdbc:sqlserver://localhost;database=Giorgia_Tech_Library");
+        dataSource.setUrl("jdbc:sqlserver://64.225.69.54;database=Giorgia_Tech_Library");
         dataSource.setUsername("librarian");
         dataSource.setPassword("userOnePassword");
         return dataSource;
@@ -36,9 +36,9 @@ public class DataSourceConfig {
     public DataSource dataSourceStudent(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSource.setUrl("jdbc:sqlserver://localhost;database=Giorgia_Tech_Library");
+        dataSource.setUrl("jdbc:sqlserver://64.225.69.54;database=Giorgia_Tech_Library");
         dataSource.setUsername("student");
-        dataSource.setPassword("123");
+        dataSource.setPassword("studentPassword");
         return dataSource;
     }
 
@@ -46,9 +46,9 @@ public class DataSourceConfig {
     public DataSource dataSourceChefLibrarian(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSource.setUrl("jdbc:sqlserver://localhost;database=Giorgia_Tech_Library");
+        dataSource.setUrl("jdbc:sqlserver://64.225.69.54;database=Giorgia_Tech_Library");
         dataSource.setUsername("chefLibrarian");
-        dataSource.setPassword("123");
+        dataSource.setPassword("chefLibrarianPassword");
         return dataSource;
     }
 
@@ -56,7 +56,7 @@ public class DataSourceConfig {
     @Bean
     @Primary
     public EntityManager entityManagerFactory() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("GiorgiaTechLibrary_Persistence-Test");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("GiorgiaTechLibrary_Persistence");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         return entityManager;
     }
